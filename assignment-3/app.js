381c56636d7de73e73ff699b11873b8ebe530322
Bob.Brehm <bob.brehm@nwea.org>
2017-12-27 17:06:09 -0800

    Service now works to display items.

--- assignment-3/app.js
+++ assignment-3/app.js
@@ -11,20 +11,22 @@
 
         var narrowItDown = this;
 
-        narrowItDown.searchItems = function(itemToSearch) {
-            MenuSearchService.getMatchedMenuItems(itemToSearch)
-                .then(function (response) {
-                    narrowItDown.found = response.data;
-                })
-                .catch(function (error) {
-                    console.log(error);
-                })
+        narrowItDown.searchItems = function() {
+            if (narrowItDown.searchTerm !== undefined) {
+                MenuSearchService.getMatchedMenuItems(narrowItDown.searchTerm)
+                    .then(function (response) {
+                        if (response.length > 0) {
+                            narrowItDown.found = response;
+                        }
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    })
+            }
         };
 
         narrowItDown.removeItem = function (itemIndex) {
-            var promise = MenuSearchService.getMatchedMenuItems();
-            shoppingList.removeItem(itemIndex);
-            this.title = origTitle + " (" + list.items.length + " items )";
+            return MenuSearchService.removeItem(itemIndex);
         };
 
     }
@@ -34,23 +36,40 @@
 
         var service = this;
         var menuItemsURL = "https://davids-restaurant.herokuapp.com/menu_items.json";
-
+        var foundItems = [];
         service.getMatchedMenuItems = function (searchTerm) {
-            var response = $http({
+
+            searchTerm = searchTerm.trim().toLowerCase();
+            foundItems.length = 0;
+
+            return $http({
                 method: "GET",
                 url: (menuItemsURL)
-            });
-
-            return response;
+            })
+                .then(function (response) {
+                    var items = response.data.menu_items;
+                    if (items.length > 0) {
+                        for (var i=0; i < items.length; i++) {
+                            if (items[i].description.toLowerCase().indexOf(searchTerm) !== -1) {
+                                foundItems.push(items[i]);
+                            }
+                        }
+                    }
+                    return foundItems;
+                })
         };
-     }
+        service.removeItem = function(itemIndex) {
+            return foundItems.splice(itemIndex, 1);
+        }
+    }
 
     function foundItemsDirective() {
         var ddo = {
             restrict: 'E',
             templateUrl: 'foundItems.html',
             scope: {
-                foundItems: '<'
+                foundItems: '<',
+                onRemove: '&'
             }
         };
 
