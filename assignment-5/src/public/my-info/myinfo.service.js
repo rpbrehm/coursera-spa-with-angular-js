a5a96dad9719d8bc9ae5be8a06066fd7e655dc0f
Bob.Brehm <bob.brehm@nwea.org>
2018-02-21 15:28:06 -0800

    Styling the myInfo display.

--- assignment-5/src/public/my-info/myinfo.service.js
+++ assignment-5/src/public/my-info/myinfo.service.js
@@ -5,8 +5,13 @@
         .service('MyinfoService', MyinfoService);
 
     MyinfoService.injectg = ['$http', 'MenuService'];
+
     function MyinfoService($http, MenuService) {
-            
+         var myInfoService = this;
+         var myInfo = {};
+         myInfo.firstName = "";
+         myInfo.lastName = "";
+
     }
     
 })();
